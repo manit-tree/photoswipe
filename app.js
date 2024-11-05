e0f5823fa9f83a9222feb21ed78cf833f927bbd7
8columns <manit.tree@gmail.com>
2024-11-05 20:31:04 +0700

    update

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ import Photoswipe from './dist/index.es.js';
 
 document.addEventListener('DOMContentLoaded', evt => {
     let el = document.querySelector('#gallery');
-
     Photoswipe(el, {children: 'a'});
 })
